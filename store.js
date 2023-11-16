fdc0d375f4782b0dc85e1e6a30aa2119e3a453bc
Veronica-Void <vvoid8075@gmail.com>
2023-11-16 07:53:55 -0800

    manager login and sign up

--- store.js
+++ store.js
@@ -1,14 +1,48 @@
-function userCheck(){
-    let ask = prompt('Are you a Soiree Guest or a Manager?')
-    if(ask == 'guest' || ask == 'Guest'){
-        alert('Taking you to guest page')
-        location.replace('guestSignUp.html')
-    }
-    else if(ask == 'manager' || ask == 'Manager'){
-        alert('Redirecting to manager page')
-        location.replace('mSignUp.html')
+// customer sign up and log in functions
+function signUp(){
+    let email = document.getElementById('email').value
+    let pass = document.getElementById('pass').value
+    console.log(email,pass)
+    localStorage.setItem(email,pass)
+    alert('Account created successfully')
+}
+function login(){
+    let email = document.getElementById('email').value
+    let pass = document.getElementById('pass').value
+    if(localStorage.getItem(email)){
+        if(pass === localStorage.getItem(email)){
+            alert('Log in success.')
+            location.replace('menu.html')
+        }
+        else{
+            alert('Invalid password')
+        }
     }
     else{
-        alert('Please insert a valid option')
+        alert('Invalid username')
+    }
+}
+// manager sign up and login  functions
+function managerSign(){
+    let email = document.getElementById('email').value
+    let pin = document.getElementById('pin').value
+    console.log(email,pin)
+    localStorage.setItem(email,pin)
+    alert('Account created successfully')
+}
+function managerlog(){
+    let email = document.getElementById('email').value
+    let pin = document.getElementById('pin').value
+    if(localStorage.getItem(email)){
+        if(pin = localStorage.getItem('pin')){
+            alert('Log in success.')
+            location.replace('managerDirectory.html')
+        }
+        else{
+            alert('Invalid Manager Pin')
+        }
+    }
+    else{
+        alert('Invalid username')
     }
 }
