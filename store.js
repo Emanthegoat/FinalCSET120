99d0597ff7e792cd09d222d04c49acb5ef4a3611
Veronica-Void <vvoid8075@gmail.com>
2023-11-21 04:47:02 -0800

    added log in and sign up button, added comments to js

--- store.js
+++ store.js
@@ -1,13 +1,18 @@
-
-// customer sign up and log in functions
+// variables for functions
+var CustomerLogin = {email: "123@t.com", password: "12345"}
+var ManagerLogin= {email: "123@t.com", password: "12345"}
+var SignupInfo = {email: "", password: ""}
+// General sign up function
 function signUp(){
     let email = document.getElementById('email').value
     let pass = document.getElementById('pass').value
     console.log(email,pass)
-    localStorage.setItem(email,pass)
     alert('Account created successfully')
 }
+// log in function for customer and manager
 function login(){
+    localStorage.setItem('email', CustomerLogin.email)
+    localStorage.setItem('pass', ManagerLogin.email)
     let pin = document.getElementById('pin').value
     let email = document.getElementById('email').value
     let pass = document.getElementById('pass').value
@@ -27,40 +32,20 @@ function login(){
         alert('Invalid username')
     }
 }
-// manager sign up and login functions
-function managerSign(){
-    let email = document.getElementById('email').value
-    let pin = document.getElementById('pin').value
-    console.log(email,pin)
-    localStorage.setItem(email,pin)
-    alert('Account created successfully')
-}
-function managerlog(){
-    
-    if(localStorage.getItem(email)){
-        if(pin = localStorage.getItem('pin')){
-            alert('Log in success.')
-            location.replace('Manager_Directory.html')
-        }
-        else{
-            alert('Invalid Manager Pin')
-        }
-    }
-    else{
-        alert('Invalid username')
-    }
-}
-function myPassword(){
-    var x = document.getElementById("input");
-    if (x.type === "password") {
-        x.type = "text" ;
-    }
-        else {
-            x.type = "password";
-        }
-    }
 
 
-var CustomerLogin = {email: "123@t.com", password: "12345"}
-var ManagerLogin= {email: "123@t.com", password: "12345"}
-var SignupInfo = {email: "", password: ""}
+
+
+
+// function myPassword(){
+//     var x = document.getElementById("input");
+//     if (x.type === "password") {
+//         x.type = "text" ;
+//     }
+//         else {
+//             x.type = "password";
+//         }
+//     }
+
+
+
