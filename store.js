f9f3a0f103981fcca4a1173ac2d41fcbf28a2ad2
O-Will-O <winderswilliam122205@gmail.com>
2023-12-04 09:31:18 -0500

    added login

--- store.js
+++ store.js
@@ -1,20 +1,20 @@
 var NumberOfUsers = 0;
 //all the login info for the manager
-localStorage.setItem("ManagerUser", 'Manager')
-localStorage.setItem("ManagerEmail", 'Manager@sugarsoiree.yummy')
+localStorage.setItem("ManagerUser", 'manager')
+localStorage.setItem("ManagerEmail", 'manager@sugarsoiree.yummy')
 localStorage.setItem("ManagerPassword", 'Qwerty1234')
 localStorage.setItem("ManagerPin", '7791')
 //user: manager  email:Manager@sugarsoiree.yummy  password: qwerty1234  pin: 7791
 
 //all the login info for the already existing customer
-localStorage.setItem("ExistCustomerUser", '')
-localStorage.setItem("ExistCustomerEmail", '')
-localStorage.setItem("ExistCustomerPassword", '')
+localStorage.setItem("ExistCustomerUser", 'johndoe!!')
+localStorage.setItem("ExistCustomerEmail", 'johndoe!@fak3.nam3')
+localStorage.setItem("ExistCustomerPassword", 'IWantYummyFood123')
 
 
 
 
-//make it so that multiple users can be created and are saved differently
+//make it so that stored logins are converted to lowercase
 
 
 
@@ -80,9 +80,9 @@ function registerCustomer()
     else
     {
         NumberOfUsers++;
-        localStorage.setItem(`Customer${NumberOfUsers}  Username`, user);
+        localStorage.setItem(`Customer${NumberOfUsers}  Username`, user.toLowerCase());
         localStorage.setItem(`Customer${NumberOfUsers}  Password`, pass);
-        localStorage.setItem(`Customer${NumberOfUsers}  Email`, email);
+        localStorage.setItem(`Customer${NumberOfUsers}  Email`, email.toLowerCase());
         localStorage.setItem(`Customer${NumberOfUsers}  Name`, name)
         localStorage.setItem("CurrentLogin", "customer")
         location.replace('index.html');
@@ -103,24 +103,72 @@ function registerCustomer()
 }
 
 // // log in function for customer and manager - manager part needs to be added
-function login(){
-    // const user = document.getElementById('username').value 
-    // const pass = document.getElementById('password').value
-    // if(localStorage.getItem(user) == user){
-    //     if(pass == localStorage.getItem(pass)){
-    //         alert('Successful Log-in!')
-    //         location.replace('menu.html')
-    //     }
-    //     else{
-    //         alert('Invalid password entered')
-    //     }
-    // }
-    // else{
-    //     alert('Invalid username entered')
-    // }
+function login()
+{
     const userOrEmail = document.getElementById('Username-Or-Email').value
     const loginPass = document.getElementById('Login-Password').value
-
+    userOrEmail.toLowerCase()
+    //check for manager email match
+    if(userOrEmail == localStorage.getItem('ManagerUser') || userOrEmail == localStorage.getItem('ManagerEmail'))//check 
+    {
+        if(loginPass === localStorage.getItem('ManagerPassword'))
+        {
+            alert('Login Successful')
+            localStorage.setItem("CurrentLogin", "Manager")
+            return location.href = 'Manager_Directory.html';
+        }
+        else
+        {
+            alert("Incorrect Username or Password")
+            document.getElementById('Login-Password').innerText = "";
+            document.getElementById('Username-Or-Email').innerText = "";
+        }
+    }
+    else if(userOrEmail == localStorage.getItem('ExistCustomerUser') || userOrEmail == localStorage.getItem('ExistCustomerEmail'))//check existing cutomer email and user
+    {
+        if(loginPass === localStorage.getItem('ExistCustomerPassword'))
+        {
+            alert('Login Successful')
+            localStorage.setItem("CurrentLogin", "customer")
+            return location.href = 'index.html';
+        }
+        else
+        {
+            alert("Incorrect Username or Password")
+            document.getElementById('Login-Password').innerText = "";
+            document.getElementById('Username-Or-Email').innerText = "";
+        }
+    }
+    else if(userOrEmail !== localStorage.getItem('ManagerUser') || userOrEmail !== localStorage.getItem('ManagerEmail') || userOrEmail !== localStorage.getItem('ExistCustomerUser') || userOrEmail !== localStorage.getItem('ExistCustomerEmail'))
+    {
+        //check stored emails
+        for(let i = 0;i < NumberOfUsers.length; i++)
+        {
+            if(userOrEmail == localStorage.getItem(`Customer${i} Username`) || userOrEmail == localStorage.getItem(`Customer${i} Email`))
+            {
+                if(loginPass === localStorage.getItem(`Customer${i} Password`))
+                {
+                    alert('Login Successful')
+                    localStorage.setItem("CurrentLogin", "customer")
+                    return location.href = 'index.html';
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+        alert("Incorrect Username or Password")
+        document.getElementById('Login-Password').innerText = "";
+        document.getElementById('Username-Or-Email').innerText = "";
+    }
+    else
+    {
+        if(confirm("Account does not exist. Please Consider Registering"))
+        {
+            location.href = "signUp.html";
+        }
+    }
 }
 //Checks and makes sure that the document is loaded before we access the different parts of it
 if(document.readyState == "loading") 
